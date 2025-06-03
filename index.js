986b7509a78c38c3eb28f5e7a5dd9c10895ea2c1
Masood Shuja <masood@antiquelogic.com>
2025-06-03 02:48:34 -0700

    initial commit - project setup

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      callback(null, origin);
+    },
+    credentials: true,
+  })
+);
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).json({ message: "hello" });
+});
+
+app.listen(PORT);
