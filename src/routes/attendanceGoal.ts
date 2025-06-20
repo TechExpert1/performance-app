f49b48cddeb09b5def3ab14311706b338a995946
Masood Shuja <masood@antiquelogic.com>
2025-06-19 23:53:42 -0700

    Set group by date goals

--- src/routes/attendanceGoal.ts
+++ src/routes/attendanceGoal.ts
@@ -8,6 +8,12 @@ router.get("/home-stats", userAuth, attendanceGoalController.homeStats);
 router.post("/", userAuth, attendanceGoalController.create);
 router.patch("/:id", userAuth, attendanceGoalController.update);
 router.delete("/:id", userAuth, attendanceGoalController.remove);
-router.get("/:id", attendanceGoalController.getById);
+
 router.get("/", userAuth, attendanceGoalController.getAll);
+router.get(
+  "/type-grouped",
+  userAuth,
+  attendanceGoalController.attendanceGoalsGroupedByType
+);
+router.get("/:id", attendanceGoalController.getById);
 export default router;
