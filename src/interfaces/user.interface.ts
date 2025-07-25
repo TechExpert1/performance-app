e68cdd76396a0c4aa505fb9412baea4141aa7069
Masood Shuja <masood@antiquelogic.com>
2025-07-25 05:24:37 -0700

    set get profile, add gym details

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -12,8 +12,7 @@ export interface IUser {
   role: "superAdmin" | "gymOwner" | "coach" | "athlete";
   profileImage?: string;
   referralSource?: string;
-  gym?: string;
-  gymOwner?: Types.ObjectId; // for coach
+  gym?: Types.ObjectId; // for coach
   createdAt?: Date;
   updatedAt?: Date;
 }
